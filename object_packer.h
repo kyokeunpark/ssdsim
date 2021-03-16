e6f632e37184af9ba2dd84a3101665b20a7cf988
KyoKeun Park <kyokeun.park@mail.utoronto.ca>
2021-03-16 09:25:53 -0400

    Implemented more to object packer -- still needs a lot of work

--- object_packer.h
+++ object_packer.h
@@ -1,16 +1,21 @@
 #ifndef __OBJECT_PACKER_H_
 #define __OBJECT_PACKER_H_
 
+#pragma once
+
 #include <unordered_map>
 #include <utility>
 #include <vector>
 #include "object_manager.h"
 #include "extent_manager.h"
+#include "extent_stack.h"
 #include "extent_object.h"
 #include "extent.h"
 
-using object_lst = std::vector<std::pair<Extent_Object*, int>>;
+using obj_record = std::pair<Extent_Object*, int>;
+using object_lst = std::vector<obj_record>;
 using current_extents = std::unordered_map<int, Extent*>;
+using ext_types_mgr = std::unordered_map<string, int>;
 
 /*
  * Interface for ObjectPackers to follow
@@ -35,10 +40,12 @@ public:
 
 class GenericObjectPacker: public ObjectPacker {
 
+protected:
 	ObjectManager obj_manager;
 	ExtentManager ext_manager;
 	object_lst obj_pool;
 	current_extents current_exts;
+	ext_types_mgr ext_types;
 	short threshold, num_objs_in_pool;
 	bool record_ext_types;
 
@@ -49,7 +56,10 @@ public:
 			short num_objs_in_pool = 100, short threshold = 10, bool record_ext_types = false)
 		: obj_manager(obj_manager), ext_manager(ext_manager), obj_pool(obj_pool),
 		current_exts(current_exts), num_objs_in_pool(num_objs_in_pool),
-		threshold(threshold), record_ext_types(record_ext_types) {}
+		threshold(threshold), record_ext_types(record_ext_types)
+		{
+			this->ext_types = ext_types_mgr();
+		}
 
 	/*
 	 * Add the object to the object pool. Need to specify the size since when
@@ -89,6 +99,70 @@ public:
 			}
 		}
 	}
+
+	/*
+	 * Returns the extent type based on its largest object. Right now large
+	 * extents have objects that occupy the whole extent (i.e., object is
+	 * larger than one extent). Small obj extents that have objects that are
+	 * smaller than the gc_threshold. The rest are defined by the percentage
+	 * occupancy of the extent by the largest object.
+	 */
+	string get_extent_type(Extent * extent)
+	{
+		return ""
+	}
+
+	void update_extent_type(Extent * extent)
+	{
+		if (this->record_ext_types) {
+		}
+	}
+
+	void add_obj_to_current_ext_at_key(ExtentStack extent_stack, Extent_Object * obj,
+			int obj_rem_size, int key)
+	{
+		int temp = 0;
+		if (this->current_exts.find(key) == this->current_exts.end())
+			this->current_exts[key] = this->ext_manager.create_extent();
+		Extent * current_ext = this->current_exts[key];
+
+		while (obj_rem_size > 0) {
+			temp = current_ext->add_object(obj, obj_rem_size);
+			obj_rem_size -= temp;
+
+			// Seal the extent if the extent is full
+			if (obj_rem_size > 0 || (obj_rem_size == 0 && current_ext->free_space == 0)) {
+			}
+		}
+	}
+
+	/*
+	 * Method that retrieves all objects from object pool. Identifies the
+	 * corresponding key for each object in current_extents and calls
+	 * add_obj_to_current_ext_at_key.
+	 */
+	virtual void pack_objects(ExtentStack extent_stack) = 0;
+	// TODO: extent_stack might need to be a pointer here
+};
+
+class SimpleObjectPacker: public GenericObjectPacker {
+
+public:
+
+	SimpleObjectPacker(ObjectManager & obj_manager, ExtentManager & ext_manager,
+			object_lst obj_pool = object_lst(), current_extents current_exts = current_extents(),
+			short num_objs_in_pool = 100, short threshold = 10, bool record_ext_types = false)
+		: GenericObjectPacker(obj_manager, ext_manager, obj_pool, current_extents,
+				num_objs_in_pool, threshold, record_ext_types) {}
+
+	void pack_objects(ExtentStack extent_stack)
+	{
+		while(this->obj_pool.size > 0) {
+			obj_record obj = this->obj_pool.pop_back();
+		}
+		(void) extent_stack;
+	};
+
 };
 
 #endif // __OBJECT_PACKER_H_
