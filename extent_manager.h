8de714d8815db58b120db045e0774c8f5f313266
KyoKeun Park <kyokeun.park@mail.utoronto.ca>
2021-03-29 22:06:26 -0400

    Initialize exts list for extent manager

--- extent_manager.h
+++ extent_manager.h
@@ -4,34 +4,38 @@
 #include <unordered_map>
 
 class ExtentManager {
-  public:
-    int ext_size;
-    list<Extent *> *exts;
-    float (Extent::*key_fnc)();
-    ExtentManager() {}
-    ExtentManager(int s, float (Extent::*k_f)()) : ext_size(s), key_fnc(k_f) {}
-    float get_key(Extent *ext) { return (ext->*key_fnc)(); }
-    int get_num_ext() { return exts->size(); }
+public:
+  int ext_size;
+  list<Extent *> exts;
+  float (Extent::*key_fnc)();
 
-    Extent *create_extent(int s = 0, int secondary_threshold = 15) {
-        Extent *e;
-        if (s == 0) {
-            e = new Extent(ext_size, secondary_threshold);
-        } else {
-            e = new Extent(s, secondary_threshold);
-        }
-        exts->push_back(e);
-        return e;
+  ExtentManager() {}
+  ExtentManager(int s, float (Extent::*k_f)())
+      : ext_size(s), key_fnc(k_f), exts(list<Extent *>()) {}
+
+  float get_key(Extent *ext) { return (ext->*key_fnc)(); }
+
+  int get_num_ext() { return exts.size(); }
+
+  Extent *create_extent(int s = 0, int secondary_threshold = 15) {
+    Extent *e;
+    if (s == 0) {
+      e = new Extent(ext_size, secondary_threshold);
+    } else {
+      e = new Extent(s, secondary_threshold);
     }
+    exts.push_back(e);
+    return e;
+  }
 
-    unordered_map<string, int> get_ext_types() {
-        unordered_map<string, int> ret;
-        for (Extent *e : *exts) {
-            ret.emplace(e->type, 0);
-            ret[e->type] += 1;
-        }
-        return ret;
+  unordered_map<string, int> get_ext_types() {
+    unordered_map<string, int> ret;
+    for (Extent *e : exts) {
+      ret.emplace(e->type, 0);
+      ret[e->type] += 1;
     }
+    return ret;
+  }
 
-    void delete_extent(Extent *extent) { exts->remove(extent); }
+  void delete_extent(Extent *extent) { exts.remove(extent); }
 };
