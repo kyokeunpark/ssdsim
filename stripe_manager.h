dd19e4bc27c8b9c4c5dcedfd137bcf6b323693bb
reemo <siyuew1@uci.edu>
2021-03-30 21:15:12 -0400

    fixed bug in stripe manager getdatadcsize and object manager deleteobject

--- stripe_manager.h
+++ stripe_manager.h
@@ -1,64 +1,72 @@
 #pragma once
 #include "config.h"
 #include "extent_object_stripe.h"
+#include <cstdio>
 // get_extents(stripe id) not used anywhere not implemented
 // member variable ext_size_to_stripes not used anywhere not implemented
 // getters not implemented since variables are public
 class StripeManager {
-public:
-  list<Stripe *> *stripes;
-  int num_data_exts_per_locality;
-  float num_local_parities;
-  float num_global_parities;
-  int num_localities_in_stripe;
-  int num_exts_per_stripe;
-  int num_data_exts_per_stripe;
-  float coding_overhead;
-  int max_id;
+  public:
+    list<Stripe *> *stripes;
+    int num_data_exts_per_locality;
+    float num_local_parities;
+    float num_global_parities;
+    int num_localities_in_stripe;
+    int num_exts_per_stripe;
+    int num_data_exts_per_stripe;
+    float coding_overhead;
+    int max_id;
 
-  StripeManager(int num_data_extents, int num_local_parities,
-                int num_global_parities, int num_localities_in_stripe,
-                int coding_overhead = 0)
-      : stripes(new list<Stripe *>()),
-        num_data_exts_per_locality(num_data_extents),
-        num_local_parities(num_local_parities),
-        num_global_parities(num_global_parities),
-        num_localities_in_stripe(num_localities_in_stripe),
-        coding_overhead(coding_overhead), max_id(0) {
-    num_exts_per_stripe =
-        num_data_exts_per_locality * num_localities_in_stripe +
-        num_local_parities + num_global_parities;
-    num_data_exts_per_stripe =
-        num_data_exts_per_locality * num_localities_in_stripe;
-  }
-
-  double get_data_dc_size() {
-    int dc_size = 0;
-    for (Stripe *stripe : *stripes) {
-      dc_size += stripe->ext_size * num_data_exts_per_stripe;
+    StripeManager(int num_data_extents, float num_local_parities,
+                  float num_global_parities, int num_localities_in_stripe,
+                  float coding_overhead)
+        : stripes(new list<Stripe *>()),
+          num_data_exts_per_locality(num_data_extents),
+          num_local_parities(num_local_parities),
+          num_global_parities(num_global_parities),
+          num_localities_in_stripe(num_localities_in_stripe), max_id(0) {
+        num_exts_per_stripe =
+            num_data_exts_per_locality * num_localities_in_stripe +
+            num_local_parities + num_global_parities;
+        num_data_exts_per_stripe =
+            num_data_exts_per_locality * num_localities_in_stripe;
+        if (coding_overhead == 0.0) {
+            this->coding_overhead = (num_global_parities + num_local_parities +
+                                     num_data_exts_per_stripe) /
+                                    num_data_exts_per_stripe;
+        } else {
+            this->coding_overhead = coding_overhead;
+        }
     }
-    fprintf(stderr, "dc size: %f %d\n", configtime, dc_size);
-    return dc_size;
-  }
 
-  double get_total_dc_size() {
-    fprintf(stderr, "dc total size: %f %f\n", configtime,
-            get_data_dc_size() * coding_overhead);
-    return get_data_dc_size() * coding_overhead;
-  }
+    double get_data_dc_size() {
+        int dc_size = 0;
+        for (Stripe *stripe : *stripes) {
+            dc_size += stripe->ext_size * num_data_exts_per_stripe;
+        }
+        fprintf(stderr, "dc size: %f %d\n", configtime, dc_size);
+        return dc_size;
+    }
 
-  int get_num_stripes() { return stripes->size(); }
+    double get_total_dc_size() {
+        fprintf(stderr, "dc total size: %f %f coding overhead: %f\n",
+                configtime, get_data_dc_size() * coding_overhead,
+                coding_overhead);
+        return get_data_dc_size() * coding_overhead;
+    }
 
-  Stripe *create_new_stripe(int ext_size) {
-    // not translated if block since ext_size local variable cant be found
-    // or assigned anywhere else in stripe manager object!! if (ext_size is
-    // None):
-    //  ext_size = self.ext_size
-    Stripe *stripe = new Stripe(max_id++, num_data_exts_per_locality,
-                                num_localities_in_stripe, ext_size, 15);
-    stripes->push_back(stripe);
-    return stripe;
-  }
+    int get_num_stripes() { return stripes->size(); }
 
-  void delete_stripe(Stripe *stripe) { stripes->remove(stripe); }
+    Stripe *create_new_stripe(int ext_size) {
+        // not translated if block since ext_size local variable cant be found
+        // or assigned anywhere else in stripe manager object!! if (ext_size is
+        // None):
+        //  ext_size = self.ext_size
+        Stripe *stripe = new Stripe(max_id++, num_data_exts_per_locality,
+                                    num_localities_in_stripe, ext_size, 15);
+        stripes->push_back(stripe);
+        return stripe;
+    }
+
+    void delete_stripe(Stripe *stripe) { stripes->remove(stripe); }
 };
--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "configs.h"
+#include "stripe_manager.h"
 #include "gtest/gtest.h"
 #include <iostream>
 
@@ -27,9 +28,62 @@ TEST(ObjectManagerTest, CreateThreeNewObject) {
                       make_shared<DeterministicDistributionSampler>(365));
     object_lst objs = o_m.create_new_object(3);
     EXPECT_EQ(objs.size(), 3);
-    for (auto &kv : objs) {
-        cout << kv.second << endl;
-    }
+};
+
+TEST(ObjectManagerTest, GetSingleObjectById) {
+    ObjectManager o_m =
+        ObjectManager(make_shared<EventManager>(),
+                      make_shared<DeterministicDistributionSampler>(365));
+    object_lst objs = o_m.create_new_object(1);
+    EXPECT_EQ(objs.size(), 1);
+    EXPECT_EQ(o_m.get_object(0), objs[0].first);
+};
+TEST(ObjectManagerTest, GetMultipleObjectsById) {
+    ObjectManager o_m =
+        ObjectManager(make_shared<EventManager>(),
+                      make_shared<DeterministicDistributionSampler>(365));
+    object_lst objs = o_m.create_new_object(3);
+    EXPECT_EQ(objs.size(), 3);
+    EXPECT_EQ(o_m.get_object(0), objs[0].first);
+    EXPECT_EQ(o_m.get_object(1), objs[1].first);
+    EXPECT_EQ(o_m.get_object(2), objs[2].first);
+};
+
+TEST(ObjectManagerTest, DeleteObject) {
+    ObjectManager o_m =
+        ObjectManager(make_shared<EventManager>(),
+                      make_shared<DeterministicDistributionSampler>(365));
+    object_lst objs = o_m.create_new_object(3);
+    EXPECT_EQ(objs.size(), 3);
+    EXPECT_EQ(o_m.get_object(0), objs[0].first);
+    o_m.remove_object(o_m.get_object(0));
+    EXPECT_EQ(o_m.get_object(0), nullptr);
+};
+
+TEST(StripeManagerTest, GetDataDcSizeANDGetTotalDataDcSize) {
+    StripeManager s_m = StripeManager(7, 2, 2, 2, 0.0);
+    s_m.create_new_stripe(5);
+    s_m.create_new_stripe(10);
+    EXPECT_NEAR(s_m.get_data_dc_size(), 210, 3);
+    EXPECT_NEAR(s_m.get_total_dc_size(), 270, 3);
+};
+
+TEST(StripeManagerTest, GetDataDcSizeANDGetTotalDataDcSizeFLOATS) {
+    StripeManager s_m = StripeManager(1, 2.0 / 14, 2.0 / 14, 1, 18.0 / 14);
+    s_m.create_new_stripe(5);
+    s_m.create_new_stripe(10);
+    EXPECT_NEAR(s_m.get_data_dc_size(), 15, 3);
+    EXPECT_NEAR(s_m.get_total_dc_size(), 19.28571428571429, 3);
+};
+
+TEST(StripeManagerTest, DeleteStripe) {
+    StripeManager s_m = StripeManager(1, 2.0 / 14, 2.0 / 14, 1, 18.0 / 14);
+    s_m.create_new_stripe(5);
+    s_m.create_new_stripe(10);
+    Stripe *s = s_m.stripes->front();
+    s_m.delete_stripe(s);
+    EXPECT_EQ(find(s_m.stripes->begin(), s_m.stripes->end(), s),
+              s_m.stripes->end());
 };
 
 int main(int argc, char **argv) {
